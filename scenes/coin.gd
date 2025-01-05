7f7d9fd06069f41b7cbfc2f0fb82f942c566b00c
Switel12 <31106054+Switel12@users.noreply.github.com>
2025-01-05 17:01:19 +0100

    Just comments
    Just added some comments to the code for me if I don't know what's for what

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-signal coin_collected
+signal coin_collected # Creating signal?
 
+# I don't know if the function is needed but I'll leave it
 func _process(delta: float) -> void:
 	pass
 
 
+# This function is called when body entered area2d
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	emit_signal("coin_collected")
-	queue_free()
+	emit_signal("coin_collected") # Emmiting signal to other nodes?
+	queue_free() # Removing coin from scene if is pick up
--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,12 +1,12 @@
 extends CanvasLayer
 
-var coins = 0
+var coins = 0 # Number of coins thats been pick up
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = "POINTS: " + str(coins)
+	$Label.text = "POINTS: " + str(coins) # Change of value at each frame if the player has collected coins
 
 
-
+# Signal from coin.gd script if player entered coin then adds +1 to coin value
 func _on_node_2d_coin_collected() -> void:
 	coins = coins + 1
